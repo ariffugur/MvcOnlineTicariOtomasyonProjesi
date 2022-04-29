6029271eaa8fb96fa9c8483a5b10c28c43b27df9
ariffugur <ariffugur@gmail.com>
2022-04-29 06:53:21 +0300

    Urun Detay

--- MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -12,8 +12,11 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c=new Context();
         public ActionResult Index()
         {
-            var degerler=c.Uruns.Where(x=>x.UrunId==1).ToList();
-            return View(degerler);
+            Class1 cs=new Class1();
+            //var degerler=c.Uruns.Where(x=>x.UrunId==1).ToList();
+            cs.Deger1=c.Uruns.Where(x=>x.UrunId==1).ToList();   
+            cs.Deger2=c.Detays.Where(y=>y.DetayId==1).ToList();   
+            return View(cs);
         }
     }
 }
