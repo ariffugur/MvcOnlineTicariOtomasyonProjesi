df0fbebb7288b0e786d59976e51295ec17bffae2
ariffugur <ariffugur@gmail.com>
2022-05-11 19:06:50 +0300

    Personel Gorsel

--- MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,6 +32,14 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpPost]
         public ActionResult PersonelEkle(Personel p)
         {
+            if (Request.Files.Count > 0)
+            {
+                string dosyaAdi=Path.GetFileName(Request.Files[0].FileName);
+                string uzanti=Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaAdi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                p.PersonelGorsel = "/Image/" + dosyaAdi + uzanti;
+            }
            c.Personels.Add(p);
             c.SaveChanges();
             return RedirectToAction("Index");
@@ -49,6 +58,14 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         }
         public ActionResult PersonelGuncelle(Personel p)
         {
+            if (Request.Files.Count > 0)
+            {
+                string dosyaAdi = Path.GetFileName(Request.Files[0].FileName);
+                string uzanti = Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaAdi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                p.PersonelGorsel = "/Image/" + dosyaAdi + uzanti;
+            }
             var prs = c.Personels.Find(p.PersonelId);
             prs.PersonelAd= p.PersonelAd;
             prs.PersonelSoyad= p.PersonelSoyad;
