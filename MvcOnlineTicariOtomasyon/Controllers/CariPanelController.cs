548544779053eccca2c14d62d84b47a168b617d9
ariffugur <ariffugur@gmail.com>
2022-05-18 22:44:21 +0300

    Mesaj Detalari

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -8,22 +8,61 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CariPanelController : Controller
     {
-        Context c=new Context();
+        Context c = new Context();
         // GET: CariPanel
         [Authorize]
         public ActionResult Index()
         {
             var mail = (string)Session["CariMail"];
-            var degerler=c.Carilers.FirstOrDefault(x=>x.CariMail==mail);
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
             ViewBag.m = mail;
             return View(degerler);
         }
         public ActionResult Siparislerim()
         {
             var mail = (string)Session["CariMail"];
-            var id=c.Carilers.Where(x=>x.CariMail==mail.ToString()).Select(y=>y.CariId).FirstOrDefault();
-            var degerler=c.SatisHarekets.Where(x=>x.CariId==id).ToList();
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.CariId).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.CariId == id).ToList();
             return View(degerler);
         }
+        public ActionResult GelenMesajlar()
+        {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.Mesajlars.Where(x => x.Alici == mail).ToList();
+            var gelenSayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            var gidenSayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidenSayisi;
+            ViewBag.d1 = gelenSayisi;
+            return View(mesajlar);
+        }
+        public ActionResult GidenMesajlar()
+        {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).ToList();
+            var gelenSayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelenSayisi;
+            var gidenSayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidenSayisi;
+            return View(mesajlar);
+        }
+        public ActionResult MesajDetay()
+        {
+            var mail = (string)Session["CariMail"];
+            var gelenSayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            var gidenSayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gidenSayisi;
+            ViewBag.d1 = gelenSayisi;
+            return View();
+        }
+        //[HttpGet]
+        //public ActionResult YeniMesaj()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult YeniMesaj()
+        //{
+        //    return View();
+        //}
     }
 }
