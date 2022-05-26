d17a9d4473f9c0691969b994e0a7b0d6c8f7c083
ariffugur <ariffugur@gmail.com>
2022-05-26 05:08:12 +0300

    DropDown

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -12,9 +12,9 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     {
         // GET: Kategori
         Context c = new Context();
-        public ActionResult Index(int sayfa=1)
+        public ActionResult Index(int sayfa = 1)
         {
-            var degerler = c.Kategoris.ToList().ToPagedList(sayfa,4);
+            var degerler = c.Kategoris.ToList().ToPagedList(sayfa, 4);
             return View(degerler);
         }
         [HttpGet] //Form yüklendiği zaman ilk bu alanı çalıştır.
@@ -38,15 +38,34 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         }
         public ActionResult KategoriGetir(int id)
         {
-            var ktg= c.Kategoris.Find(id);
-            return View("KategoriGetir",ktg);
-        }        
+            var ktg = c.Kategoris.Find(id);
+            return View("KategoriGetir", ktg);
+        }
         public ActionResult KategoriGuncelle(Kategori k)
         {
-            var ktg= c.Kategoris.Find(k.KategoriID);
+            var ktg = c.Kategoris.Find(k.KategoriID);
             ktg.KategoriAd = k.KategoriAd;
             c.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult Deneme()
+        {
+            Class3 cs = new Class3();
+            cs.Kategoriler = new SelectList(c.Kategoris, "KategoriID", "KategoriAd");
+            cs.Urunler = new SelectList(c.Uruns, "UrunId", "UrunAd");
+            return View(cs);
+        }
+        public JsonResult UrunGetir(int p)
+        {
+            var urunListesi = (from x in c.Uruns
+                               join y in c.Kategoris on x.Kategori.KategoriID equals y.KategoriID
+                               where x.Kategori.KategoriID == p
+                               select new
+                               {
+                                    Text=x.UrunAd,
+                                    Value=x.UrunId.ToString()
+                               }).ToList();
+            return Json(urunListesi,JsonRequestBehavior.AllowGet);
         }
     }
 }
